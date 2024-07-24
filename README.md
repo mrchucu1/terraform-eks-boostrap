5b3eb864350ee1f20eb8c30654b011ed311279ba
Diego Navarro <diegonavarro400@gmail.com>
2024-07-24 08:32:36 -0600

    Scaffold created

---
+++ README.md
@@ -0,0 +1,6 @@
+# Terraform create EKS module
+
+This is a basic EKS creation module.
+
+# LICENCE
+see LICENCE, GNU GENERAL PUBLIC LICENSE.
---
+++ variables.tf
@@ -0,0 +1,41 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-west-2"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  default     = "my-eks-cluster"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  default     = "10.0.0.0/16"
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for EKS worker nodes"
+  default     = "t3.medium"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  default     = "kubernetes-cluster"
+}
+
+variable "s3_state" {
+  description = "S3 bucket for your terraform state"
+  default     = "terraform-state-63c9063cdb15"
+}
